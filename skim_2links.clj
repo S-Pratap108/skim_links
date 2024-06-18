3143612e9dc95916212904e7149071cbd76c1ce9
S.Pratap <s.pratap@ada-asia.com>
2024-06-18 16:26:49 +0530

    updated ccd

--- skim_2links.clj
+++ skim_2links.clj
@@ -1,31 +1,32 @@
 (config
     (text-field
         :name "clientId" 
-        :label "Enter clientId"
+        :label "Client Id"
         :placeholder "Enter Client Id"
         :required true
-        (api-config-field)
     )
+
     (password-field
         :name "clientSecret" 
-        :label "Enter clientSecret"
+        :label "Client Secret"
         :placeholder "Enter client secret"
         :required true
-        (api-config-field :masked true)
     )
+
     (text-field
-        :name "PublisherId" 
+        :name "publisherId" 
         :label "Publisher Id"
         :placeholder "Enter Publisher Id"
         :required true
     )
-    (oauth2/authorization-code-flow-client-credentials
+
+    (oauth2/refresh-token-with-client-credentials
         (token
             (source
                 (http/post
                     :url "https://authentication.skimapis.com/access_token"
                     (body-params
-                    "response_type" "code"
+                    ;; "response_type" "code"
                     "client_id" "{clientId}"
                     "client_secret" "{clientSecret}"
                     "grant_type" "client_credentials"
@@ -34,13 +35,12 @@
                 )
             )
             (fields
-                access_token :<= "access_token"
+                access_token
                 refresh_token
-                token_type :<= "token_type"
-                scope :<= "scope"
-                scope 
+                token_type
+                scope
                 realm_id :<= "realmId"
-                expires_in :<= "expires_in"
+                expires_in :<= "expiry_timestamp"
             )
         )
     )
@@ -48,23 +48,28 @@
 (default-source (http/get :base-url "https://merchants.skimapis.com/v4"
     (header-params "Accept" "application/json"))
     (paging/offset  :offset-query-param-initial-value 0
-                    offset-query-param-name "offset"
-                    limit 200
-                    limit-query-param-name "limit")
-    (auth/oauth2)
+                    :offset-query-param-name "offset"
+                    :limit 200
+                    :limit-query-param-name "limit")
+    (auth/oauth2
+        (auth-params
+             (query-params 
+                 "access_token"  "{access_token}")))
     (error-handler
-        (when :status 490 :message "Too Many Requests")
+        (when :status 429 :action rate-limit)
+        (when :status 401 :action refresh)
     )
 )
 
-(entity MERCHANT 
-        "represents a merchant program having an active relationship with the authenticated user in Skimlinks' system"
-        (api-docs-url "https://developers.skimlinks.com/merchant.html")
-        (source (http/get :url "/publisher/{PublisherId}/merchants")
-                (extract-path "merchants")
-                (query-params "publisher_id" "{PublisherId}"
-                              "access_token" "{access_token}"
-                              "publisher_domain_id" )
-        )
+(temp-entity MERCHANT 
+    (api-docs-url "https://developers.skimlinks.com/merchant.html")
+    (source (http/get :url "/publisher/{publisherId}/merchants")
+            (query-params "publisher_id" "{publisherId}"
+                            "publisher_domain_id" )
+            (setup-test
+                (upon-receiving :code 200 :action (pass)))
+            (extract-path "merchants"))    
+(fields
+    id :id)
 )
 
